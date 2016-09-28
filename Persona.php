a104a9af9cb6da8d0ad7d9634edd4a36ab8b7452
MarcPetit92 <1001489.joan23@fje.edu>
2016-09-28 18:42:58 +0200

    Add files via upload

---
+++ Persona.php
@@ -0,0 +1,30 @@
+
+<?php
+		class Persona{
+
+						//Atributos
+
+						public $name = "Pedro";
+						public $surname = "Garcia";
+
+						//public function mostrar(){
+
+						//	echo $this->name. " " . $this->surname;
+							
+						
+			                
+			               $personita = new  Persona();
+			               echo $personita -> name; 
+
+			               //metodo especial constructor
+			               public function __construct($name,$surname){
+
+
+			               		$this->name=$name;
+			            }
+       			  }
+
+       			  $personita = new Persona("Pedro","Garcia")
+
+       			  
+?>
